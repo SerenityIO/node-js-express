44127378a791a5f0ec13713b5362d9bb1715a489
Dmytro Sapozhnyk <dimonsuper010@gmail.com>
2018-08-28 11:55:47 +0300

    Created add

---
+++ app.js
@@ -0,0 +1,75 @@
+import express from 'express'
+import createError from 'http-errors'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import http from 'http'
+
+import indexRouter from './routes/index'
+import usersRouter from './routes/users'
+
+import nconf from './config';
+import mongoose from './lib/mongoose.js'
+
+const app = express();
+
+const server = http.createServer(app);
+
+
+server.listen(nconf.get('port'));
+server.on('error', onError);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;  
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
